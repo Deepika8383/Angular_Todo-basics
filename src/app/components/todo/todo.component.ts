e21b8a82118910d4fce4c7c2c11543cebec077e3
Deepika8383 <117986650+Deepika8383@users.noreply.github.com>
2024-04-25 15:03:50 +0530

    Update todo.component.ts

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface Todo {
   content: string;
@@ -11,18 +13,29 @@ interface Todo {
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   todos: Todo[] = [];
-  inputTodo: string = '';
+  todoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      inputTodo: ['', Validators.required],
+      completed: [false],
+      content: ['']
+    });
+  }
 
   addTodo() {
-    if (this.inputTodo.trim()) {
+    if (this.todoForm.valid) {
+      const todo = this.todoForm.value;
       this.todos.push({
-        content: this.inputTodo,
-        completed: false,
+        content: todo.inputTodo,
+        completed: todo.completed,
         editMode: false
       });
-      this.inputTodo = '';
+      this.todoForm.reset();
     } else {
       alert('Please enter a task before adding!');
     }
@@ -41,10 +54,71 @@ export class TodoComponent {
   }
 
   toggleEditMode(index: number) {
-    if (this.todos[index].editMode) {
-      this.todos[index].editMode = false;
+    const todo = this.todos[index];
+    if (todo.editMode) {
+
+      todo.content = this.todoForm.value.content;
+      console.log(this.todoForm.value.content);
+      todo.editMode = false;
     } else {
-      this.todos[index].editMode = true;
+      
+      this.todoForm.patchValue({
+        content: todo.content
+      });
+      console.log(this.todoForm.value.content);
+      todo.editMode = true;
     }
   }
+  
 }
+
+// import { Component } from '@angular/core';
+
+// interface Todo {
+//   content: string;
+//   completed: boolean;
+//   editMode: boolean;
+// }
+
+// @Component({
+//   selector: 'app-todo',
+//   templateUrl: './todo.component.html',
+//   styleUrls: ['./todo.component.css']
+// })
+// export class TodoComponent {
+//   todos: Todo[] = [];
+//   inputTodo: string = '';
+
+//   addTodo() {
+//     if (this.inputTodo.trim()) {
+//       this.todos.push({
+//         content: this.inputTodo,
+//         completed: false,
+//         editMode: false
+//       });
+//       this.inputTodo = '';
+//     } else {
+//       alert('Please enter a task before adding!');
+//     }
+//   }
+
+//   toggleDone(index: number) {
+//     this.todos[index].completed = !this.todos[index].completed;
+//   }
+
+//   deleteTodo(index: number) {
+//     this.todos.splice(index, 1);
+//   }
+
+//   toggleContent(index: number) {
+//     this.todos[index].editMode = true;
+//   }
+
+//   toggleEditMode(index: number) {
+//     if (this.todos[index].editMode) {
+//       this.todos[index].editMode = false;
+//     } else {
+//       this.todos[index].editMode = true;
+//     }
+//   }
+// }
